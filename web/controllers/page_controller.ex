db697c19be152058a84fa6c8214bf5481678cf0a
devansjo <dan@thebookofeveryone.com>
2016-11-03 00:36:22 +0100

    make layouts work

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,6 +6,6 @@ defmodule Tboechatbot.PageController do
     user_id = conn.assigns.user_id
     app_token = Application.get_env(:tboechatbot, :smooch_app_token)
     chat_api_url = Application.get_env(:tboechatbot, :chat_api_url)
-    render conn, "index.html", %{user_id: user_id, app_token: app_token, chat_api_url: chat_api_url}
+    render conn, "index.html", %{header: "header.html", footer: "footer.html", user_id: user_id, app_token: app_token, chat_api_url: chat_api_url}
   end
 end
